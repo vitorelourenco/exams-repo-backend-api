767577cfca995c09d01b05a68b1e961dc84cb212
Vitor Lourenco <vitor.registros@lourencos.net>
2021-07-29 08:32:52 -0300

    Make aws sign route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
+import './setup';
 import express from "express";
 import cors from "cors";
+import AWS from 'aws-sdk';
+import fs from 'fs';
 
 const app = express();
 app.use(cors());
@@ -7,6 +10,33 @@ app.use(express.json());
 
 app.get("/test", (req, res) => {
   res.send("OK!");
+});
+
+app.get('/sign-s3', (req, res) => {
+  console.log("ayayayayayyyya")
+  const s3 = new AWS.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${process.env.AWS_S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
 });
 
 export default app;
