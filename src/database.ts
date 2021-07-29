e5b68b341fbd2ee8db5ee0aa0b97c31b8c052a6f
Vitor Lourenco <vitor.registros@lourencos.net>
2021-07-29 10:32:36 -0300

    Add ssl config to db connection

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,17 @@ import { getConnectionManager } from "typeorm";
 const DATABASE_URL = process.env.DATABASE_URL;
 
 export default async function connect() {
-  const connectionManager = await getConnectionManager();
+  const connectionManager = getConnectionManager();
   const connection = connectionManager.create({
     name: "default",
     type: "postgres",
     url: DATABASE_URL,
-    entities: ["src/entities/*.ts"]
+    entities: ["src/entities/*.ts"],
+    extra: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   });
   await connection.connect();
   return connection;
