27ccc75436619a7bc64f4c20eaccde9ea6802648
Vitor Lourenco <vitor.registros@lourencos.net>
2021-07-31 11:29:27 -0300

    Include exams in get instructor with id response

--- src/services/instructorServices.ts
+++ src/services/instructorServices.ts
@@ -2,6 +2,6 @@ import { getRepository } from "typeorm";
 import Course from "../entities/Course";
 
 export async function getWith(courseId:number) {
-  const courses = await getRepository(Course).findOne({where:{id:courseId}, relations:["instructors"]});
+  const courses = await getRepository(Course).findOne({where:{id:courseId}, relations:["instructors","instructors.exams"]});
   return courses.instructors;
 }
