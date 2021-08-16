23b5e598b8dfa06715042c5d517bac0b9daba685
Vitor Lourenco <vitor.registros@lourencos.net>
2021-08-16 19:05:41 -0300

    Remove dummy text

--- tests/factories/courseFactory.ts
+++ tests/factories/courseFactory.ts
@@ -7,8 +7,6 @@ import CreateCourse from '../../src/protocols/CreateCourse';
 import Period from '../../src/entities/Period';
 import Degree from '../../src/entities/Degree';
 
-banana
-
 export async function createCourse(params: {
 	name: string;
 	instructors: Instructor[];
